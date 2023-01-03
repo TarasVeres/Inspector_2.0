3487db011c3936180bd8c7d7e10faa26f2391064
BUN_TAR <84289323+TarasVeres@users.noreply.github.com>
2023-01-03 09:02:06 +0200

    Initial commit

---
+++ Inline_Keyboard.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+from aiogram import Bot, Dispatcher, types
+
+Token_work = '5388966053:AAE6rJo_7wbBbGDMG3QntbjN549Ym1lyEgY'
+Chat_work = '-1001286473377'
+
+Token_test = '5182014508:AAEBytjLM9Gu-3F2o1Qc2QPt5bwdvNWxFEk'
+Chat_test = '-1001626029923'
